193f32355fecb8e43dd0a6920f774b426463ef1e
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-10-09 00:34:27 +0600

    testimonial section and footer section completed

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,33 @@
 
 const Blog = () => {
     return (
-        <div>
-             
+        <div className="py-20 grid grid-cols-1 px-10">
+            <div className="card card-side bg-slate-800 shadow-xl mb-5">
+                <figure><img src="https://i.ibb.co/zFDQwnz/i.jpg" alt="Movie" className="w-[280px] h-full"/></figure>
+                <div className="card-body">
+                    <p>November 20, 2022 | 8 comments</p>
+                    <h2 className="card-title">5 Common mistakes you avoid while Choosing an Event Management Company</h2>
+                    <p>Choosing an event management company can be tricky but worry not, we are here with all the information you need to make this process easy. Event managers are professionals with skills to manage an event, they have resources, access, and contacts. Finding the right event management company is not easy these days, but you can do it if you avoid the following 5 common mistakes.</p>
+                </div>
+            </div>
+            <div className="card card-side bg-slate-800 shadow-xl mb-5">
+                <figure><img src="https://i.ibb.co/tDbScqc/12345.jpg" alt="Movie" className="w-[280px] h-full"/></figure>
+                <div className="card-body">
+                    <p>October 12, 2022 | 7 comments</p>
+                    <h2 className="card-title">Choosing Top 5 Event Management Companies on the basis of the Following Causes</h2>
+                    <p>Event management is a thrilling career option if you are authentic, creative, and passionate about hosting people then this could be a wonderful career option for you.
+
+If you are planning a corporate or family event and need help in executing your ideas then you can always hire event management companies. While we can not choose the best event management companies we can always tell you how you can choose event management companies.</p>
+                </div>
+            </div>
+            <div className="card card-side bg-slate-800 shadow-xl">
+                <figure><img src="https://i.ibb.co/3YbP5yk/event-planner.jpg" alt="Movie" className="w-[280px] h-full"/></figure>
+                <div className="card-body">
+                    <p>January 8, 2022  |  No Comments </p>
+                    <h2 className="card-title">Qualities of Professional Event Management Agencies</h2>
+                    <p>Event management needs the synchronized effort of various organizing, planning, and other executive skills. Event management agencies are a set of teams that deals with the client’s needs and take care of and organize events such as fashion shows, cooperate events, wedding receptions, fundraisers, and many more events that we host during many occasions. They have an excellent set of tools that explores various brands and many products to organize certain even at the use of budget. </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/EventServices.jsx
+++ src/pages/Home/EventServices.jsx
@@ -5,8 +5,8 @@ const EventServices = () => {
     const events = useLoaderData();
     return (
         <div>
-            <h2 className="text-center text-xl font-bold">Our Services</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 mt-10">
+            <h2 className="text-center text-2xl font-bold">Our Services</h2>
+            <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
                 {
                     events.map(event => <SingleEvent key={event.id} event={event}></SingleEvent>)
                 }
