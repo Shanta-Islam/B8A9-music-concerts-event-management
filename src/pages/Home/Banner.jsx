b857c500b3b0320ddff939fdb8929b94ec1e9eee
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-11-09 21:13:36 +0600

    some changes

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -8,52 +8,6 @@ import 'swiper/css/scrollbar';
 const Banner = () => {
     return (
         <div className='py-20'>
-            {/* <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <div className="relative w-full mx-auto lg:mt-10">
-                        <img className="h-[500px] w-full object-cover rounded-lg" src="https://i.ibb.co/BLSkFc6/hero-image1.jpg" alt="Random image"/>
-                            <div className="absolute inset-0 bg-gray-950  opacity-60 rounded-lg"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <div className="text-center">
-                                    <h2 className="text-white text-2xl lg:text-7xl font-bold">Exotic tastes at your <br/> doorstep
-                                    </h2>
-                                    <p className="text-white text-sm lg:text-xl font-normal lg:font-medium lg:mt-6">We supply highly
-                                        quality organic products</p>
-                                    <button className="btn bg-fruit-primary text-white border-0 hover:bg-red-500  mt-4 lg:mt-8">Shop
-                                        Now</button>
-                                </div>
-                            </div>
-                    </div>
-                    <div
-                        className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 left-3 lg:right-5 right-3 top-1/2">
-                        <a href="#slide2" className="btn btn-circle bg-[#121212] text-gray-50 border-0">❮</a>
-                        <a href="#slide2" className="btn btn-circle bg-[#121212] text-gray-50 border-0">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <div className="relative w-full mx-auto lg:mt-10">
-                        <img className="h-[500px] w-full object-cover rounded-lg" src="https://i.ibb.co/6F2jyBH/hero-image2.jpg" alt="Random image"/>
-                            <div className="absolute inset-0 bg-gray-950 opacity-60 rounded-lg"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <div className="text-center">
-                                    <h2 className="text-white text-2xl lg:text-7xl font-bold">Exotic tastes at your <br/> doorstep
-                                    </h2>
-                                    <p className="text-white text-sm lg:text-xl font-normal lg:font-medium lg:mt-6">We supply highly
-                                        quality organic products</p>
-                                    <button className="btn bg-fruit-primary text-white border-0 hover:bg-red-500 mt-4 lg:mt-8">Shop
-                                        Now</button>
-                                </div>
-                            </div>
-                    </div>
-                    <div
-                        className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 left-3 lg:right-5 right-3 top-1/2">
-                        <a href="#slide1" className="btn btn-circle bg-[#121212] text-gray-50 border-0">❮</a>
-                        <a href="#slide1" className="btn btn-circle bg-[#121212] text-gray-50 border-0">❯</a>
-                    </div>
-                </div>
-                
-                
-            </div> */}
             <Swiper
                 modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
                 spaceBetween={50}
